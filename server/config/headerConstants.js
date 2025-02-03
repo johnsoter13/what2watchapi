a60603478165b142032292af0bb0693fdbf41265
jsoter <jsoter@cisco.com>
2025-02-02 21:07:21 -0500

    fix: updated backend and added websocket

--- server/config/headerConstants.js
+++ server/config/headerConstants.js
@@ -5,9 +5,13 @@ export const IMDB_HEADERS = {
   'Content-Type': 'application/json; charset=utf-8',
 }
 
-export const STREAMING_AVAILABILITY = {
+export const STREAMING_AVAILABILITY_HEADERS = {
   'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
   'x-rapidapi-key': '6fe1ca1b03msh55fda2ba57be013p1f9236jsn0080a63ac940',
   useQueryString: true,
   'Content-Type': 'application/json; charset=utf-8',
+}
+
+export const GOOGLE_AUTH_HEADERS = {
+  'Content-Type': 'application/json',
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import { WebSocketServer } from 'ws'
+import { createServer } from 'http'
+
 import middlewareConfig from './config/middleware'
-import { IMDB_HEADERS, STREAMING_AVAILABILITY } from './config/headerConstants'
+import {
+  IMDB_HEADERS,
+  STREAMING_AVAILABILITY_HEADERS,
+  GOOGLE_AUTH_HEADERS,
+} from './config/headerConstants'
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
+const server = createServer(app) // Create an HTTP server to use with WebSocket
+
 const FETCH_GENRE_URL = 'https://streaming-availability.p.rapidapi.com/genres'
 const FETCH_MOVIES_BY_GENRE_URL =
   'https://streaming-availability.p.rapidapi.com/shows/search/filters'
@@ -14,6 +23,7 @@ const FETCH_MEDIA_FROM_MOVIE_ID =
   'https://streaming-availability.p.rapidapi.com/shows'
 const FETCH_MOST_POPULAR =
   'https://imdb236.p.rapidapi.com/imdb/most-popular-movies'
+const LOGIN_URL = 'https://oauth2.googleapis.com/tokeninfo'
 
 // Database
 // dbConfig();
@@ -24,6 +34,18 @@ middlewareConfig(app)
 // app.use('/api', [What2WatchRoutes]);
 // Proxy endpoints
 app.use(
+  '/login',
+  createProxyMiddleware({
+    target: LOGIN_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/login`]: '',
+    },
+    headers: GOOGLE_AUTH_HEADERS,
+  })
+)
+
+app.use(
   '/fetchGenres',
   createProxyMiddleware({
     target: FETCH_GENRE_URL,
@@ -31,7 +53,7 @@ app.use(
     pathRewrite: {
       [`^/fetchGenres`]: '',
     },
-    headers: STREAMING_AVAILABILITY,
+    headers: STREAMING_AVAILABILITY_HEADERS,
   })
 )
 
@@ -43,7 +65,7 @@ app.use(
     pathRewrite: {
       [`^/fetchMoviesByGenre`]: '',
     },
-    headers: STREAMING_AVAILABILITY,
+    headers: STREAMING_AVAILABILITY_HEADERS,
   })
 )
 
@@ -67,7 +89,7 @@ app.use(
     pathRewrite: {
       [`^/fetchMediaFromMovieId`]: '',
     },
-    headers: STREAMING_AVAILABILITY,
+    headers: STREAMING_AVAILABILITY_HEADERS,
   })
 )
 
@@ -79,7 +101,7 @@ app.use(
     pathRewrite: {
       [`^/fetchMediaFromSearch`]: '',
     },
-    headers: STREAMING_AVAILABILITY,
+    headers: STREAMING_AVAILABILITY_HEADERS,
   })
 )
 
@@ -97,11 +119,77 @@ app.use(
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, (err) => {
+// Create a WebSocket server
+const wss = new WebSocketServer({ server })
+
+// A map to keep track of clients in each room
+const rooms = {}
+
+wss.on('connection', (ws) => {
+  let currentRoomId = null
+
+  ws.on('message', (message) => {
+    const parsedMessage = JSON.parse(message)
+    const { type, roomId, movieId, userName, liked } = parsedMessage
+
+    switch (type) {
+      case 'join':
+        currentRoomId = roomId
+        if (!rooms[roomId]) {
+          rooms[roomId] = new Set()
+        }
+        rooms[roomId].add(ws)
+        ws.send(
+          JSON.stringify({
+            message: `${userName} Joined room ${roomId}`,
+            roomSize: rooms[currentRoomId].size,
+          })
+        )
+        break
+      case 'message':
+        if (currentRoomId && rooms[currentRoomId]) {
+          rooms[currentRoomId].forEach((client) => {
+            if (client !== ws && client.readyState === 1) {
+              client.send(
+                JSON.stringify({
+                  userName,
+                  roomId: currentRoomId,
+                  movieId,
+                  roomSize: rooms[currentRoomId].size,
+                  liked,
+                })
+              )
+            }
+          })
+        }
+        break
+      case 'leave':
+        rooms[roomId].delete(ws)
+        if (rooms[roomId].size === 0) {
+          delete rooms[currentRoomId]
+        }
+        currentRoomId = null
+        ws.send(JSON.stringify({ message: `${userName} Left room ${roomId}` }))
+        break
+      default:
+        ws.send(JSON.stringify({ error: 'Unknown message type' }))
+    }
+  })
+
+  ws.on('close', () => {
+    if (currentRoomId && rooms[currentRoomId]) {
+      rooms[currentRoomId].delete(ws)
+      if (rooms[currentRoomId].size === 0) {
+        delete rooms[currentRoomId]
+      }
+    }
+  })
+})
+
+server.listen(PORT, (err) => {
   if (err) {
     console.error(err)
-  }
-  {
-    console.log(`App listen to port: ${PORT}`)
+  } else {
+    console.log(`App listening on port: ${PORT}`)
   }
 })
